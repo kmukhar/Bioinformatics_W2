5578eeebf075e462f69b4ea569c73b389971d0d3
kmukhar <kevin.mukhar@gmail.com>
2013-11-25 22:47:31 -0800

    finished leaderboard

--- Bioinformatics_W2/src/CycloPeptideSequencing.java
+++ Bioinformatics_W2/src/CycloPeptideSequencing.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class CycloPeptideSequencing {
 	static String[] aminos = new String[] { "G", "A", "S", "P", "V", "T", "C",
@@ -18,11 +21,12 @@ public class CycloPeptideSequencing {
 			Peptide.getPeptide("R"), Peptide.getPeptide("Y"),
 			Peptide.getPeptide("W") };
 
+	SpectrumGenerator sg = new SpectrumGenerator();
+
 	public ArrayList<String> sequence(ArrayList<Integer> input) {
 		ArrayList<String> result = new ArrayList<>();
 		ArrayList<String> queue = new ArrayList<>();
 		queue.add("");
-		SpectrumGenerator sg = new SpectrumGenerator();
 
 		while (!queue.isEmpty()) {
 			queue = expandQueue(queue);
@@ -54,25 +58,114 @@ public class CycloPeptideSequencing {
 
 	public ArrayList<String> leaderboardSequence(ArrayList<Integer> spectrum,
 			int maxCandidates) {
-		ArrayList<String> leaders = new ArrayList<>();
+		CopyOnWriteArrayList<String> leaders = new CopyOnWriteArrayList<>();
 		ArrayList<String> result = new ArrayList<>();
 		String leader = "";
 		leaders.add(leader);
+		int parentMass = spectrum.get(spectrum.size() - 1);
 		// while Leaderboard is non-empty
 		while (!leaders.isEmpty()) {
-//        Leaderboard <- Expand(Leaderboard)
-//        for each Peptide in Leaderboard
-//            if Mass(Peptide) = ParentMass(Spectrum)
-//                if Score(Peptide, Spectrum) > Score(LeaderPeptide, Spectrum)
-//                    LeaderPeptide <- Peptide
-//            else if Mass(Peptide) > ParentMass(Spectrum)
-//                remove Peptide from Leaderboard
-//        Leaderboard <- Cut(Leaderboard, Spectrum, N)
-//    output LeaderPeptide
+			leaders = expandQueue(leaders);
+			// for each Peptide in Leaderboard
+			for (String s : leaders) {
+				int mass = getMass(s);
+				if (mass == parentMass) {
+					int sScore = Score(s, spectrum);
+					int lScore = Score(leader, spectrum);
+					if (sScore > lScore) {
+						leader = s;
+						result.clear();
+						result.add(s);
+					} else if (sScore == lScore) {
+						result.add(s);
+					}
+				} else if (mass > parentMass) {
+					// remove Peptide from Leaderboard
+					leaders.remove(s);
+				}
+			}
+			leaders = Cut(leaders, spectrum, maxCandidates);
 		}
+
+		System.out.println("leader: " + leader);
+//		leaders = CutToScore(leaders, spectrum, Score(leader, spectrum));
+//		result.addAll(leaders);
+		result = convertToWeights(result);
+
 		return result;
 	}
-    
+
+	private CopyOnWriteArrayList<String> CutToScore(
+			CopyOnWriteArrayList<String> leaders, ArrayList<Integer> spectrum,
+			int i) {
+
+		TreeMap<Integer, ArrayList<String>> ordered = new TreeMap<>();
+		for (String s : leaders) {
+			int score = Score(s, spectrum);
+			ArrayList<String> candidates = ordered.get(score);
+			if (candidates == null)
+				candidates = new ArrayList<>();
+			candidates.add(s);
+			ordered.put(score, candidates);
+		}
+
+		leaders.clear();
+
+		Integer key = ordered.lastKey();
+		ArrayList<String> candidates = ordered.get(key);
+		leaders.addAll(candidates);
+		return leaders;
+	}
+
+	private int Score(String s, ArrayList<Integer> spectrum) {
+		ArrayList<Integer> candidate = sg.generateSpectrum(s, true);
+		ArrayList<Integer> target = new ArrayList<>(spectrum.size());
+		target.addAll(spectrum);
+		int result = 0;
+
+		for (Integer i : candidate)
+			if (target.remove(i))
+				++result;
+		return result;
+	}
+
+	private CopyOnWriteArrayList<String> Cut(
+			CopyOnWriteArrayList<String> leaders, ArrayList<Integer> spectrum,
+			int maxCandidates) {
+
+		if (leaders.size() <= maxCandidates)
+			return leaders;
+
+		TreeMap<Integer, ArrayList<String>> ordered = new TreeMap<>();
+		for (String s : leaders) {
+			int score = Score(s, spectrum);
+			ArrayList<String> candidates = ordered.get(score);
+			if (candidates == null)
+				candidates = new ArrayList<>();
+			candidates.add(s);
+			ordered.put(score, candidates);
+		}
+
+		leaders.clear();
+
+		for (Integer key : ordered.descendingKeySet()) {
+			ArrayList<String> candidates = ordered.get(key);
+			leaders.addAll(candidates);
+			if (leaders.size() >= maxCandidates)
+				break;
+		}
+		return leaders;
+	}
+
+	private int getMass(String s) {
+		int sum = 0;
+		for (int i = 0; i < s.length(); i++) {
+			Peptide p = Peptide.getPeptide(s.substring(i, i + 1));
+			sum += p.getWeight();
+		}
+		return sum;
+	}
+
 	private ArrayList<String> convertToWeights(ArrayList<String> result) {
 		ArrayList<String> converted = new ArrayList<>();
 		for (String s : result) {
@@ -94,6 +187,17 @@ public class CycloPeptideSequencing {
 	private ArrayList<String> expandQueue(ArrayList<String> queue) {
 		ArrayList<String> result = new ArrayList<>(queue.size() * 18);
 		for (String s : queue)
+			for (Peptide p : peptides) {
+				String es = s + p.value;
+				result.add(es);
+			}
+		return result;
+	}
+
+	private CopyOnWriteArrayList<String> expandQueue(
+			CopyOnWriteArrayList<String> leaders) {
+		CopyOnWriteArrayList<String> result = new CopyOnWriteArrayList<>();
+		for (String s : leaders)
 			for (Peptide p : peptides) {
 				String es = s + p.value;
 				result.add(es);
--- Bioinformatics_W2/src/CycloPeptideSequencingTest.java
+++ Bioinformatics_W2/src/CycloPeptideSequencingTest.java
@@ -84,6 +84,7 @@ public class CycloPeptideSequencingTest {
 		ArrayList<String> actual = c.sequence(input);
 		System.out.println(actual.toString().replace(",", ""));
 	}
+
 	@Test
 	public void testSequence05() {
 		In in = new In("Tyrocidine_B1_theoretical_spectrum_1.txt");
@@ -94,6 +95,96 @@ public class CycloPeptideSequencingTest {
 		in.close();
 
 		ArrayList<String> actual = c.sequence(input);
+		System.out.println(actual.toString().replace(",", ""));
+	}
+
+	@Test
+	public void testLeader01() {
+		In in = new In("leaderboard_sample.txt");
+		in.readLine();
+		String s = in.readLine();
+		int maxCandidates = Integer.parseInt(s);
+
+		s = in.readLine();
+		ArrayList<Integer> spectrum = new ArrayList<>();
+		for (String s2 : s.split(" "))
+			spectrum.add(Integer.parseInt(s2));
+
+		in.readLine();
+		ArrayList<String> expected = new ArrayList<>();
+		for (String s2 : in.readLine().split(" "))
+			expected.add(s2);
+		in.close();
+		Collections.sort(expected);
+
+		ArrayList<String> actual = c.leaderboardSequence(spectrum,
+				maxCandidates);
+		Collections.sort(actual);
+		System.out.println(expected);
+		System.out.println(actual);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testLeader02() {
+		In in = new In("leaderboard_data.txt");
+		in.readLine();
+		String s = in.readLine();
+		int maxCandidates = Integer.parseInt(s);
+
+		s = in.readLine();
+		ArrayList<Integer> spectrum = new ArrayList<>();
+		for (String s2 : s.split(" "))
+			spectrum.add(Integer.parseInt(s2));
+
+		in.readLine();
+		ArrayList<String> expected = new ArrayList<>();
+		for (String s2 : in.readLine().split(" "))
+			expected.add(s2);
+		in.close();
+		Collections.sort(expected);
+
+		ArrayList<String> actual = c.leaderboardSequence(spectrum,
+				maxCandidates);
+		Collections.sort(actual);
+		System.out.println(expected);
+		System.out.println(actual);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testLeader03() {
+		In in = new In("dataset_24_4.txt");
+		String s = in.readLine();
+		int maxCandidates = Integer.parseInt(s);
+
+		s = in.readLine();
+		ArrayList<Integer> spectrum = new ArrayList<>();
+		for (String s2 : s.split(" "))
+			spectrum.add(Integer.parseInt(s2));
+
+		in.close();
+
+		ArrayList<String> actual = c.leaderboardSequence(spectrum,
+				maxCandidates);
+		System.out.println(actual);
+	}
+
+	@Test
+	public void testLeader04() {
+		In in = new In("dataset_24_6.txt");
+		String s = in.readLine();
+		int maxCandidates = Integer.parseInt(s);
+
+		s = in.readLine();
+		ArrayList<Integer> spectrum = new ArrayList<>();
+		for (String s2 : s.split(" "))
+			spectrum.add(Integer.parseInt(s2));
+
+		in.close();
+
+		ArrayList<String> actual = c.leaderboardSequence(spectrum,
+				maxCandidates);
 		System.out.println(actual.toString().replace(",", ""));
 	}
 }
